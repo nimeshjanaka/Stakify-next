8b937dac75f8ab1e75ec2716c3a7ee7366f599c1
nimeshjanaka <nimeshajanaka.1995@gmail.com>
2023-11-07 15:34:03 +0530

    add new

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import CardComponent from '../components/CardComponent'
+import React from "react";
+import CardComponent from "../components/CardComponent";
 import {
   DollarOutlined,
   RedoOutlined,
@@ -7,139 +7,125 @@ import {
   BuildOutlined,
 } from "@ant-design/icons";
 
-
-
 const page = () => {
   return (
     <div>
-       
       <div className="flex  p-2 ">
         <div className="grid lg:grid-cols-3 md:grid-cols-1 grid-cols-1 gap-4 w-full ml-5 mr-5 mt-5  text-white ">
           <div>
-            <CardComponent 
-            icon={
-              <DollarOutlined
-                style={{
-                  color: "#cd4fe5",
-                  fontSize: "25px",
-                  height: "10px",
-                  marginBottom: "10px",
-                }}
-              />
-            }
-            value={"0.0000000"}
-            title={"Market Cap"}
-          />
+            <CardComponent
+              icon={
+                <DollarOutlined
+                  style={{
+                    color: "#cd4fe5",
+                    fontSize: "25px",
+                    height: "10px",
+                    marginBottom: "10px",
+                  }}
+                />
+              }
+              value={"0.0000000"}
+              title={"Market Cap"}
+            />
           </div>
           <div>
-            <CardComponent  
-            icon={
-              <RedoOutlined
-                style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
-              />
-            }
-            value={"500,000"}
-            title={"Circulating Supply"}
-          />
+            <CardComponent
+              icon={
+                <RedoOutlined
+                  style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
+                />
+              }
+              value={"500,000"}
+              title={"Circulating Supply"}
+            />
           </div>
           <div>
-            <CardComponent 
-            icon={
-              <FieldTimeOutlined
-                style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
-              />
-            }
-            value={"500,000"}
-            title={"Total Supply"}
-          />
+            <CardComponent
+              icon={
+                <FieldTimeOutlined
+                  style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
+                />
+              }
+              value={"500,000"}
+              title={"Total Supply"}
+            />
           </div>
         </div>
       </div>
-      <div className="flex justify-center p-2 bg-[#171821]">
+      <div className="flex justify-center p-2">
         <div className="grid lg:grid-cols-3 md:grid-cols-1 grid-cols-1 gap-4 w-full mt-1 ml-5 mr-5">
           <div>
-            <CardComponent 
-            icon={
-              <BuildOutlined
-                style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
-              />
-            }
-            value={"0:0:0"}
-            title={"Next Rebase"}
-          />
+            <CardComponent
+              icon={
+                <BuildOutlined
+                  style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
+                />
+              }
+              value={"0:0:0"}
+              title={"Next Rebase"}
+            />
           </div>
           <div>
-          <CardComponent
-            icon={
-              <DollarOutlined
-                style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
-              />
-            }
-            value={"0.000000"}
-            title={"Stakify ($SIFY) Price"}
-          />
+            <CardComponent
+              icon={
+                <DollarOutlined
+                  style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
+                />
+              }
+              value={"0.000000"}
+              title={"Stakify ($SIFY) Price"}
+            />
           </div>
-          
+
           <div>
-          <CardComponent
-            icon={
-              <DollarOutlined
-                style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
-              />
-            }
-            value={"4.3857"}
-            title={" Market Value of Treasury Asset"}
-          />
+            <CardComponent
+              icon={
+                <DollarOutlined
+                  style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
+                />
+              }
+              value={"4.3857"}
+              title={" Market Value of Treasury Asset"}
+            />
           </div>
         </div>
       </div>
       <div className="flex justify-center p-2">
         <div className="grid lg:grid-cols-3 md:grid-cols-1 grid-cols-1 gap-4 w-full mt-1 ml-5 mr-5 h-40">
           <div>
-          <CardComponent
-            icon={
-              <FieldTimeOutlined
-                style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
-              />
-            }
-            
-            title={" Referral Pool Value"}
-          />
+            <CardComponent
+              icon={
+                <FieldTimeOutlined
+                  style={{ color: "#cd4fe5", fontSize: "25px", height: "10px" }}
+                />
+              }
+              title={" Referral Pool Value"}
+            />
           </div>
           <div>
-          <CardComponent
-            icon={
-              <FieldTimeOutlined
-                style={{ color: "#cd4fe5", fontSize: "25px", height: "5px" }}
-              />
-            }
-            
-            title={" # Value of"}
-          />
-
+            <CardComponent
+              icon={
+                <FieldTimeOutlined
+                  style={{ color: "#cd4fe5", fontSize: "25px", height: "5px" }}
+                />
+              }
+              title={" # Value of"}
+            />
           </div>
           <div>
-          <CardComponent
-            icon={
-              <FieldTimeOutlined
-                style={{ color: "#cd4fe5", fontSize: "25px", height: "5px" }}
-              />
-            }
-            
-            title={" $ Value of"}
-          />
+            <CardComponent
+              icon={
+                <FieldTimeOutlined
+                  style={{ color: "#cd4fe5", fontSize: "25px", height: "5px" }}
+                />
+              }
+              title={" $ Value of"}
+            />
           </div>
         </div>
       </div>
     </div>
-   
-    
-     
-)
-}
+  );
+};
 
-export default page
-
-
-
-
+export default page;
